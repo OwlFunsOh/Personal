14c60986c8106363b8287cbba9333c208889de34
alfonso martinez <144267210+OwlFunsOh@users.noreply.github.com>
2024-09-06 23:14:30 -0400

    Calculator with functionality

--- webdev/calculator/script.js
+++ webdev/calculator/script.js
@@ -1,4 +1,17 @@
-result = document.getElementById("display");
-buttons = document.querySelectorAll(".button-grid button");
 
-// Add event listeners to the buttons
+
+function toDisplay(input){
+    display = document.getElementById("display");
+    display.value += input;
+}
+
+function evaluateEquation(){
+    try{
+        const display = document.getElementById('display');
+        const result = eval(display.value); // Pass the expression to evaluate as an argument
+        display.value = result;
+    }catch(error){
+        display.value = "Error";
+    }
+}
+
